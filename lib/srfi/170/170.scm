6ea81f814e88f5966f77df0048ecb5d643765ebb
Harold Ancell <hga@ancell-ent.com>
2019-07-20 14:33:51 -0500

    Code imported from lib/chibi/filesystem.stub and .scm, untested
    But it compiles.  Some changes like prepending % added, but unfinished.

--- lib/srfi/170/170.scm
+++ lib/srfi/170/170.scm
@@ -1,3 +1,12 @@
+;; Parts of this from the following copyright notices, the total has
+;; the same copyright terms, additions and changes Copyright 2019
+;; Harold Ancell, Harold Ancell assigns the rights to his additions to
+;; Alex Shinn.
+
+;; filesystem.stub -- filesystem bindings
+;; Copyright (c) 2009-2013 Alex Shinn.  All rights reserved.
+;; BSD-style license: http://synthcode.com/license.txt
+
 ;;; 3.1  Errors
 
   ;; ~~~~ Impliment!
@@ -7,6 +16,27 @@
 
 ;;; 3.3  File system
 
+;;> The fundamental directory iterator.  Applies \var{kons} to
+;;> each filename in directory \var{dir} and the result of the
+;;> previous application, beginning with \var{knil}.  With
+;;> \var{kons} as \scheme{cons} and \var{knil} as \scheme{'()},
+;;> equivalent to \scheme{directory-files}.
+
+(define (directory-fold dir kons knil)
+  (let ((dir (opendir dir)))
+    (if (not dir)
+        knil
+        (let lp ((res knil))
+          (let ((file (readdir dir)))
+            (if file
+                (lp (kons (dirent-name file) res))
+                (begin (closedir dir) res)))))))
+
+;;> Returns a list of the files in \var{dir} in an unspecified
+;;> order.
+
+(define (directory-files dir)
+  (directory-fold dir cons '()))
 
 
 ;;; 3.4  Processes
