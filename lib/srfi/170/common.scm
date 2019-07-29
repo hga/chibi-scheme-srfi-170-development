7d04561d1f7ae2a1a32904e014f82d8194ddcb77
Harold Ancell <hga@ancell-ent.com>
2019-07-29 15:16:44 -0500

    breakout delete-filesystem-object and directory-fold to common file

---
+++ lib/srfi/170/common.scm
@@ -0,0 +1,27 @@
+;; Unlike the scsh version, will raise an exception if there is a
+;; object it can't delete.  If no object, no exception.
+
+(define (delete-filesystem-object fname)
+  (if (%lstat fname)
+      (if (file-info-directory? (file-info fname #f))
+          (if (not (%delete-directory fname))
+              (errno-error (errno) delete-filesystem-object fname))
+          (if (not (%delete-file fname))
+              (errno-error (errno) delete-filesystem-object fname)))))
+
+;;> The fundamental directory iterator.  Applies \var{kons} to
+;;> each filename in directory \var{dir} and the result of the
+;;> previous application, beginning with \var{knil}.  With
+;;> \var{kons} as \scheme{cons} and \var{knil} as \scheme{'()},
+;;> equivalent to \scheme{directory-files}.
+
+(define (directory-fold dir kons knil)
+  (let ((dir (open-directory dir)))
+    (if (not dir)
+        knil
+        (let lp ((res knil))
+          (let ((file (read-directory dir)))
+            (if (not (eof-object? file))
+                (lp (kons file res))
+                (begin (close-directory dir) res)))))))
+
